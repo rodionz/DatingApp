90cfb788db24a6b4d2c9d1c5ad539d6bcf15f74d
Odiki <rodion.zinovyev@gmail.com>
2018-03-06 13:40:53 +0200

    auth

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data {
     public class AuthRepository : IAuthRepository {
@@ -9,8 +10,31 @@ namespace DatingApp.API.Data {
         public AuthRepository (DataContext context) {
             this._context = context;
         }
-        public Task<User> Login (string username, string password) {
-            throw new System.NotImplementedException ();
+        public async Task<User> Login (string username, string password) {
+            
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if(user == null)
+            return null;
+
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+             return null;
+
+             return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
+              
+              var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+              for(int i = 0; i < computedHash.Length; i++)
+              {
+                  if(computedHash[i] != passwordHash[i])
+                  return false;
+              }           
+          }
+          return true;
         }
 
         public async Task<User> Register (User user, string password) {
@@ -33,8 +57,11 @@ namespace DatingApp.API.Data {
           }
         }
 
-        public Task<bool> UserExists (string username) {
-            throw new System.NotImplementedException ();
+        public async Task<bool> UserExists (string username) {
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+              return true;
+
+              return false;
         }
     }
 }
