87447778f41d518c71dbaf5ff18f90dff6d8bc42
Odiki <rodion.zinovyev@gmail.com>
2018-04-08 16:18:44 +0300

    fix

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -12,7 +12,6 @@ namespace DatingApp.API.Data
         public DbSet<User> Users {get;set;}
         public DbSet<Photo> Photos{get;set;}
         public DbSet<Message> Messages {get;set;}
-
         public DbSet<Like> Likes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -33,14 +32,14 @@ namespace DatingApp.API.Data
                 .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<Message>()
-            .HasOne(u => u.Sender)
-            .WithMany(m => m.MessagesSent)
-            .OnDelete(DeleteBehavior.Restrict);
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
              
              builder.Entity<Message>()
-            .HasOne(u => u.Recipient)
-            .WithMany(m => m.MessageRecieved)
-            .OnDelete(DeleteBehavior.Restrict);
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessageRecieved)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
